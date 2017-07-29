5f452f0d49cf73d9265438a088619cdfea800e65
Amol Jaybhaye <25987amol@gmail.com>
2017-07-29 21:16:39 +0530

    aws key

--- web part/child login demo/faculty/fe_data_analysis/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$hostname="localhost";        
-$username="root";
-$password="";
-$dbname="student_profile";
-$department = "Computer";
-$connect= mysqli_connect($hostname,$username,$password,$dbname) or die("Error connecting to database");  
-//mysql_select_db($dbname);
-?>
