0c1ef1b4c8e6ec3ceb2ecbe58e0132d9752dd12e
mistrydennis <mistrydennis@gmail.com>
2017-07-30 00:56:00 +0530

    php script connection

--- NewMAW/app/src/main/java/com/example/unmadesai/newmaw/DbDisplay.java
+++ NewMAW/app/src/main/java/com/example/unmadesai/newmaw/DbDisplay.java
@@ -8,7 +8,9 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class DbDisplay extends AppCompatActivity {
 
@@ -38,19 +40,41 @@ public class DbDisplay extends AppCompatActivity {
 
     }
 
-    private void getJSON(String jsonUrlString) {
+    private void getJSON(String url) {
         class GetJSON extends AsyncTask<String, Void, String> {
             protected String doInBackground(String... params) {
                 String uri = params[0];
                 BufferedReader bufferedReader = null;
-                try{
-                    //HttpURLConnection
-                }catch(Exception e){}
-                return uri;
+                try {
+                    URL url = new URL(uri);
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    StringBuilder sb = new StringBuilder();
+                    bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                    String json;
+                    while ((json = bufferedReader.readLine()) != null) {
+                        sb.append(json + "\n");
+
+
+                    }
+                    return sb.toString().trim();
+
+                } catch (Exception e) {
+                    return null;
+                }
+
             }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                textView2.setText(s);
+            }
+
         }
 
-
+        GetJSON gj = new GetJSON();
+        gj.execute(url);
     }
 
 
