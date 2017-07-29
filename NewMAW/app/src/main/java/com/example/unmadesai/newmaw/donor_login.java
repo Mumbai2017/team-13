9d90c08f9a0798779d893d395b1d575686623854
unmadesai <unma96@gmail.com>
2017-07-29 23:16:28 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	MAWteam13/app/src/main/AndroidManifest.xml

--- NewMAW/app/src/main/java/com/example/unmadesai/newmaw/donor_login.java
+++ NewMAW/app/src/main/java/com/example/unmadesai/newmaw/donor_login.java
@@ -1,14 +1,32 @@
 package com.example.unmadesai.newmaw;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class donor_login extends AppCompatActivity {
-
+Button btnLogin,btnSignup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donor_login);
+
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        Intent i = new Intent(donor_login.this,wish_activity.class);
+        startActivity(i);
+    }
+});
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
 }
