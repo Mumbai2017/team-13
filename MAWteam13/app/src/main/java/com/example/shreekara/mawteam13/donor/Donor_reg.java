c4eba593f1724a9731c7319f2514ec1052c79b27
unmadesai <unma96@gmail.com>
2017-07-29 16:58:29 +0530

    donor reg donor login kid details

---
+++ MAWteam13/app/src/main/java/com/example/shreekara/mawteam13/donor/Donor_reg.java
@@ -0,0 +1,8 @@
+package com.example.shreekara.mawteam13.donor;
+
+/**
+ * Created by unma desai on 29-Jul-17.
+ */
+
+public class Donor_reg {
+}
---
+++ MAWteam13/app/src/main/java/com/example/shreekara/mawteam13/donor/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.shreekara.mawteam13.donor;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.shreekara.mawteam13.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
