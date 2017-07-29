5d0bc87f23ee40747ed14a2640f2f4c286457472
Akshayanand Raut <akshay.raut0303@gmail.com>
2017-07-29 21:31:21 +0530

    splashscreen

---
+++ MAWteam13/app/src/main/java/com/example/shreekara/mawteam13/donor/SplashScreen.java
@@ -0,0 +1,13 @@
+package com.example.shreekara.mawteam13.donor;
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+
+/**
+ * Created by Akshayanand Raut on 29-07-2017.
+ */
+
+public class SplashScreen  extends Activity {
+
+
+}
