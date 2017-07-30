a47e42ed9c254accf6f89adf174aa4808bc4b9c7
Akshayanand Raut <akshay.raut0303@gmail.com>
2017-07-30 08:09:24 +0530

    file minor updates

--- NewMAW/app/src/main/java/com/example/unmadesai/newmaw/donor_login.java
+++ NewMAW/app/src/main/java/com/example/unmadesai/newmaw/donor_login.java
@@ -1,29 +1,25 @@
 package com.example.unmadesai.newmaw;
-
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
 public class donor_login extends AppCompatActivity {
 Button btnLogin,btnSignup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donor_login);
-
         btnLogin=(Button)findViewById(R.id.btnLogin);
         btnSignup=(Button)findViewById(R.id.btnSignup);
-
         btnLogin.setOnClickListener(new View.OnClickListener() {
-    @Override
-    public void onClick(View v) {
-        Intent i = new Intent(donor_login.this,wish_activity.class);
-        startActivity(i);
-    }
-});
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(donor_login.this,wish_activity.class);
+                startActivity(i);
+            }
+        });
         btnSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,5 +31,4 @@ Button btnLogin,btnSignup;
             }
         });
     }
-
 }
