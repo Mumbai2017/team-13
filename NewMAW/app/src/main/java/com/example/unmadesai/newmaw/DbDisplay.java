00640a4051153a78c3bb7f712a864e8a132c2b11
Akshayanand Raut <akshay.raut0303@gmail.com>
2017-07-30 08:11:13 +0530

    code changes

--- NewMAW/app/src/main/java/com/example/unmadesai/newmaw/DbDisplay.java
+++ NewMAW/app/src/main/java/com/example/unmadesai/newmaw/DbDisplay.java
@@ -13,23 +13,18 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class DbDisplay extends AppCompatActivity {
-
     public TextView textView2;
     public TextView textView3;
     public Button btn;
-    public static final String jsonUrlString  = "http://10.0.2.2/team13/register.php";
-
-
+    public static final String jsonUrlString = "http://10.0.2.2/team13/register.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_db_display);
-
-        textView2 = (TextView)findViewById(R.id.textView2);
-        textView3 = (TextView)findViewById(R.id.textView3);
+        textView2 = (TextView) findViewById(R.id.textView2);
+        textView3 = (TextView) findViewById(R.id.textView3);
         btn = (Button) findViewById(R.id.btn);
-
         btn.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -37,9 +32,7 @@ public class DbDisplay extends AppCompatActivity {
                 getJSON(jsonUrlString);
             }
         });
-
     }
-
     private void getJSON(String url) {
         class GetJSON extends AsyncTask<String, Void, String> {
             protected String doInBackground(String... params) {
@@ -54,30 +47,20 @@ public class DbDisplay extends AppCompatActivity {
                     String json;
                     while ((json = bufferedReader.readLine()) != null) {
                         sb.append(json + "\n");
-
-
                     }
                     return sb.toString().trim();
 
                 } catch (Exception e) {
                     return null;
                 }
-
             }
-
             @Override
             protected void onPostExecute(String s) {
                 super.onPostExecute(s);
                 textView2.setText(s);
             }
-
         }
-
         GetJSON gj = new GetJSON();
         gj.execute(url);
     }
-
-
-
-
 }
